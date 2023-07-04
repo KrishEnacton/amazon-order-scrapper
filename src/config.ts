f63e28de39e408b098d871009900b0434a151736
Krish <kpkapadia@enacton.com>
2023-07-04 10:06:50 +0530

    feat: view order detail page html scrapped

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
 export const ScrapperConfig = {
   view_order_details_selector: 'a.yohtmlc-order-details-link',
 }
+
+export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
--- src/content/index.ts
+++ src/content/index.ts
@@ -1,23 +1,21 @@
-import { ScrapperConfig } from '../config'
+import { ScrapperConfig, sleep } from '../config'
 
 if (window.location.href.startsWith('https://www.amazon.in/gp/css/order-history')) {
   const observer = new MutationObserver(callback)
   const config = { attributes: true, childList: true, subtree: true }
   observer.observe(document.body, config)
+
   async function callback() {
-    let view_order_details_selectors = document.querySelectorAll(
+    const viewOrderDetailsSelectors = document.querySelectorAll(
       ScrapperConfig.view_order_details_selector,
     ) as any
-    if (view_order_details_selectors.length > 0) {
+    if (viewOrderDetailsSelectors.length > 0) {
       observer.disconnect()
-      let i = 0
-      while (view_order_details_selectors.length > i) {
-        const html = await fetch(view_order_details_selectors[i].href).then((res) => res.text())
+      for (const selector of viewOrderDetailsSelectors) {
+        const html = await fetch(selector.href).then((res) => res.text())
         console.log({ html })
-        i++
+        await sleep(1000)
       }
     }
   }
 }
-
-export {}
